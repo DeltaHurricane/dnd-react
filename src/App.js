b6b849a9fd58d0f2950b006223e3266d9e9fbf29
DeltaHurricane <lfferreira@daitan.com>
2019-11-01 10:07:41 -0300

    game choice page prototype working

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import Routes from './Routes';
 import CharacterContext from './contexts/characterContext';
 import './App.scss';
 import characterLoad from './services/CharacterServices';
-import getProperties from './services/getProperties';
+// import getProperties from './services/getProperties';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       character: characterLoad(),
-      racesInfo: this.racesInfo,
-      classesInfo: this.classesInfo,
+      // racesInfo: this.racesInfo,
+      // classesInfo: this.classesInfo,
     };
 
-    this.getRaces = this.getRaces.bind(this);
-    this.getClasses = this.getClasses.bind(this);
+    // this.getRaces = this.getRaces.bind(this);
+    // this.getClasses = this.getClasses.bind(this);
   }
 
-  componentDidMount() {
-    getProperties('classes').then(this.getClasses);
-    getProperties('races').then(this.getRaces);
-  }
+  // componentDidMount() {
+  //   getProperties('classes').then(this.getClasses);
+  //   getProperties('races').then(this.getRaces);
+  // }
 
-  getRaces(racesInfo) {
-    this.setState(() => ({ racesInfo }));
-  }
+  // getRaces(racesInfo) {
+  //   this.setState(() => ({ racesInfo }));
+  // }
 
-  getClasses(classesInfo) {
-    this.setState(() => ({ classesInfo }));
-  }
+  // getClasses(classesInfo) {
+  //   this.setState(() => ({ classesInfo }));
+  // }
 
   render() {
     return (
